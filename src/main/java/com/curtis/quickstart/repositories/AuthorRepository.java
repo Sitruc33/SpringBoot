2d21ca7dc476a178d83004bb2b22a5c41ea050f1
Sitruc33 <146670819+Sitruc33@users.noreply.github.com>
2025-02-05 22:17:22 -0500

    Finished move to Spring JPA

--- src/main/java/com/curtis/quickstart/repositories/AuthorRepository.java
+++ src/main/java/com/curtis/quickstart/repositories/AuthorRepository.java
@@ -1,5 +1,17 @@
 package com.curtis.quickstart.repositories;
 
-public interface AuthorRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import com.curtis.quickstart.domain.Author;
+
+@Repository
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+	Iterable<Author> ageLessThan(int age);
+	
+	@Query("Select a From Author a where a.age > ?1")
+	Iterable<Author> findAuthorsWithAgeGreaterThan(int age);
+	
 }
