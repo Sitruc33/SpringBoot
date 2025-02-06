d8e51fa80b2701e661629b0471796cf0986bb506
Sitruc33 <146670819+Sitruc33@users.noreply.github.com>
2025-02-05 21:29:54 -0500

    update to Spring JPA

--- src/main/java/com/curtis/quickstart/domain/Author.java
+++ src/main/java/com/curtis/quickstart/domain/Author.java
@@ -1,18 +1,25 @@
 package com.curtis.quickstart.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "authors")
 public class Author {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_id_seq")
 	private Long id;
 
     private String name;
---
+++ src/main/java/com/curtis/quickstart/repositories/BookRepository.java
@@ -0,0 +1,5 @@
+package com.curtis.quickstart.repositories;
+
+public interface BookRepository {
+
+}
